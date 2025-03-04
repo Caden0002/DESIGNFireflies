df6e8132ea92cf488b78a5e3b5e71e8d991ca5a6
Caden0002 <cadencqx@gmail.com>
2025-03-04 16:52:38 +0800

    change file names

--- src/components/LandingWithAnimation.jsx
+++ src/components/LandingWithAnimation.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import background from "/LandingBackground.gif"; // Import the background image
 
 const LandingWithAnimation = () => {
--- src/components/LandingWithoutAnimation.jsx
+++ src/components/LandingWithoutAnimation.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import background from "/LandingBackground.gif"; // Import the background image
 
 const LandingWithoutAnimation = () => {
